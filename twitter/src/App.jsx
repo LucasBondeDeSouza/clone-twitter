4b88bba4998403a4888ef5d5c6c0fea5307a3c4b
LucasBondeDeSouza <bondelucas77@gmail.com>
2024-09-19 12:39:09 -0300

    atualização

--- twitter/src/App.jsx
+++ twitter/src/App.jsx
@@ -63,7 +63,7 @@ export default () => {
       <Sidebar />
       <main className="flex-grow border-l border-r border-gray-700 max-w-xl">
         <header className="sticky top-0 z-10 bg-twitter-background bg-opacity-80 backdrop-blur-sm">
-          <h2 className="px-4 py-3 text-xl text-center font-bold md:text-start">For You</h2>
+          <h2 className="px-4 py-3 text-xl font-bold">For You</h2>
         </header>
 
         <TwitterForm onTweet={(content) => addNewTweet(content, Math.random() > 0.6)} />
